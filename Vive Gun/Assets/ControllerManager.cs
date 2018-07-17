b178173d5f7c53d13f8c807bc7026c5e1bb9972a
Casey Singlehurst <casey.s@live.co.uk>
2018-07-17 19:25:37 +0100

    Switching from OpenVR to Steam VR
    - Switched from OpenVR to Steam VR plugin
    - Gun Grip working (cant rotate gun while gripped)

---
+++ Vive Gun/Assets/ControllerManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour {
+
+    // 1
+    private SteamVR_TrackedObject trackedObj;
+    // 2
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    [SerializeField]
+    GameObject currentCollidingObject = null;
+    [SerializeField]
+    GameObject currentGrabbedObject = null;
+
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+
+    private void Update()
+    {
+        // 1
+        if (Controller.GetAxis() != Vector2.zero)
+        {
+            Debug.Log(gameObject.name + Controller.GetAxis());
+        }
+
+        // 2
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+        }
+
+        // 3
+        if (Controller.GetHairTriggerUp())
+        {
+            Debug.Log(gameObject.name + " Trigger Release");
+        }
+
+        // 4
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Press");
+
+            GrabOrReleaseObject();
+
+        }
+
+        // 5
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Release");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (currentCollidingObject == null)
+        {
+            currentCollidingObject = other.gameObject;
+        }
+    }
+    private void OnTriggerstay(Collider other)
+    {
+        if (currentCollidingObject == null)
+        {
+            currentCollidingObject = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (currentCollidingObject == other.gameObject)
+        {
+            currentCollidingObject = null;
+        }
+    }
+
+     void GrabOrReleaseObject()
+    {
+        if (currentGrabbedObject == null && currentCollidingObject != null)
+        {
+            currentGrabbedObject = currentCollidingObject;
+            if (currentGrabbedObject.tag == "Grip")
+            {
+                GrabGrip(currentGrabbedObject);
+            }
+        }
+        else if (currentGrabbedObject != null)
+        {
+            if (currentGrabbedObject.tag == "Grip")
+            {
+                ReleaseGrip(currentGrabbedObject);
+            }
+            currentGrabbedObject = null;
+        }
+
+
+    }
+    void GrabGrip(GameObject grip)
+    {
+        grip.gameObject.GetComponentInParent<EquipController>().gripController = this.gameObject;
+        grip.gameObject.GetComponentInParent<EquipController>().isGripped = true;
+    }
+
+    void ReleaseGrip(GameObject grip)
+    {
+        grip.gameObject.GetComponentInParent<EquipController>().gripController = null;
+        grip.gameObject.GetComponentInParent<EquipController>().isGripped = false;
+    }
+
+
+
+}
---
+++ Vive Gun/Assets/EquipController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipController : MonoBehaviour {
+
+    public GameObject controllerPosition;
+
+    public bool isEquipped = true;
+
+    float objectMoveSpeed = 20;
+
+    public Vector3  holdRotationOffset;
+
+    public GameObject grip;
+    public GameObject gripController;
+
+    public bool isGripped = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (isEquipped)
+        {
+            transform.position = Vector3.Lerp(transform.position, controllerPosition.transform.position, objectMoveSpeed * Time.deltaTime);
+            Quaternion holdRotation = new Quaternion();
+            if (isGripped)
+            {
+                Vector3 lookAtPosition = gripController.transform.position + (grip.transform.position - transform.position) ;
+
+                transform.LookAt(lookAtPosition);
+            }
+            else
+            {
+                holdRotation = controllerPosition.transform.rotation * Quaternion.Euler(holdRotationOffset);
+                transform.rotation = Quaternion.Lerp(transform.rotation, holdRotation, objectMoveSpeed * Time.deltaTime);
+            }
+
+        }
+    }
+    
+    }
+    
